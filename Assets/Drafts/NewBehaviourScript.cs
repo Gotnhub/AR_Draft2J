ba86cca501c381249ccf4110d10da980053610a5
Gotnhub <1170118106@qq.com>
2023-10-20 20:11:39 +0800

    test2

---
+++ Assets/Drafts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//射线点击周围可碰撞物体开始展开播放，再点关闭？
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# AR_Draft1J
+<<<<<<< HEAD
+school gate test AR
+？？？？？？
+=======
+school gate test AR 
+>>>>>>> 9b7341e (test4)
