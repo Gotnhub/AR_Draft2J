c51f77e4f742e040053616fa2fd35ad83ec34bc9
Yogurt <zhangyuhan0407@hotmail.com>
2023-10-31 10:03:24 +0800

    Page 2、3、4

---
+++ Assets/ARDraft/Scripts/ARCollectionView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARCollectionView : MonoBehaviour
+{
+    
+    Transform[] views;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //views = transform.children;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
